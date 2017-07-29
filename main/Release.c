d91d38a723117a22340fdb4b29fc12a53ea9f802
Fredrik Wikstrom <fredrik@a500.org>
2017-07-29 11:08:00 +0300

    Added copyright symbol.

--- main/Release.c
+++ main/Release.c
@@ -1,7 +1,7 @@
 /*
  * AmigaOS shared library implementation of libpixman-1.
  *
- * Copyright 2017 Fredrik Wikstrom <fredrik@a500.org>
+ * Copyright (C) 2017 Fredrik Wikstrom <fredrik@a500.org>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
