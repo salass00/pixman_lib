6df203497d464261d02e648492f04a4e527917f8
Fredrik Wikstrom <fredrik@a500.org>
2017-08-06 14:10:57 +0300

    Bumped revision.

--- pixman-1.library_rev.h
+++ pixman-1.library_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		53
-#define REVISION	1
-#define DATE		"29.7.2017"
-#define VERS		"pixman-1.library 53.1"
-#define VSTRING		"pixman-1.library 53.1 (29.7.2017)\r\n"
-#define VERSTAG		"\0$VER: pixman-1.library 53.1 (29.7.2017)"
+#define REVISION	2
+#define DATE		"6.8.2017"
+#define VERS		"pixman-1.library 53.2"
+#define VSTRING		"pixman-1.library 53.2 (6.8.2017)\r\n"
+#define VERSTAG		"\0$VER: pixman-1.library 53.2 (6.8.2017)"
