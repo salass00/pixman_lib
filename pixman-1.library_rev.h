228862391533aca361bfbb125e05c955b901176c
Fredrik Wikstrom <fredrik@a500.org>
2017-12-24 12:28:36 +0200

    Bumped version to 53.3.

--- pixman-1.library_rev.h
+++ pixman-1.library_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		53
-#define REVISION	2
-#define DATE		"6.8.2017"
-#define VERS		"pixman-1.library 53.2"
-#define VSTRING		"pixman-1.library 53.2 (6.8.2017)\r\n"
-#define VERSTAG		"\0$VER: pixman-1.library 53.2 (6.8.2017)"
+#define REVISION	3
+#define DATE		"24.12.2017"
+#define VERS		"pixman-1.library 53.3"
+#define VSTRING		"pixman-1.library 53.3 (24.12.2017)\r\n"
+#define VERSTAG		"\0$VER: pixman-1.library 53.3 (24.12.2017)"
