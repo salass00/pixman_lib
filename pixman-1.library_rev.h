1c91ef7e558493ffe263fb14db0c47e691a9838b
Fredrik Wikstrom <fredrik@a500.org>
2022-11-29 21:15:30 +0200

    Bumped version to 53.5.

--- pixman-1.library_rev.h
+++ pixman-1.library_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		53
-#define REVISION	4
-#define DATE		"20.12.2018"
-#define VERS		"pixman-1.library 53.4"
-#define VSTRING		"pixman-1.library 53.4 (20.12.2018)\r\n"
-#define VERSTAG		"\0$VER: pixman-1.library 53.4 (20.12.2018)"
+#define REVISION	5
+#define DATE		"29.11.2022"
+#define VERS		"pixman-1.library 53.5"
+#define VSTRING		"pixman-1.library 53.5 (29.11.2022)\r\n"
+#define VERSTAG		"\0$VER: pixman-1.library 53.5 (29.11.2022)"
